15fec7407a853c96be8822fd588b0c4790c0f903
Jaswanth <81845691+Jaswanth28@users.noreply.github.com>
2022-12-14 07:37:13 +0530

    Update wj.py

--- wj.py
+++ wj.py
@@ -1,18 +1,20 @@
 from collections import defaultdict
+c=0
 jug1, jug2, aim = 4, 3, 2
 visited = defaultdict(lambda: False)
-def waterJugSolver(amt1, amt2):
-    if (amt1 == aim and amt2 == 0) or (amt2 == aim and amt1 == 0):
-        print(amt1, amt2)
+def waterJugSolver(amt1, amt2,c):
+    c+=1
+    if (amt1 == aim and amt2 == aim):
+        #print(amt1, amt2)
+        print("minimum no of steps:",c)
         return True
     if visited[(amt1, amt2)] == False:
-        print(amt1, amt2)
+        #print(amt1, amt2)
         visited[(amt1, amt2)] = True
-        return (waterJugSolver(0, amt2) or waterJugSolver(amt1, 0) or waterJugSolver(jug1, amt2) or
-                        waterJugSolver(amt1, jug2) or 
-                        waterJugSolver(amt1 + min(amt2, (jug1-amt1)),amt2 - min(amt2, (jug1-amt1))) or
-                        waterJugSolver(amt1 - min(amt1, (jug2-amt2)),amt2 + min(amt1, (jug2-amt2))))
+        return (waterJugSolver(0, amt2,c) or waterJugSolver(amt1, 0,c) or waterJugSolver(jug1, amt2,c) or
+                        waterJugSolver(amt1, jug2,c) or 
+                        waterJugSolver(amt1 + min(amt2, (jug1-amt1)),amt2 - min(amt2, (jug1-amt1)),c) or
+                        waterJugSolver(amt1 - min(amt1, (jug2-amt2)),amt2 + min(amt1, (jug2-amt2)),c))
     else:
         return False
-print("Steps: ")
-waterJugSolver(0, 0)
+waterJugSolver(0, 0,c)
